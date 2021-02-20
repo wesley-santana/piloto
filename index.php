385a810936f4f2e4f25d9ff904be3ac60de82313
Wesley de Santana <use.wesley@gmail.com>
2021-02-20 10:46:58 -0300

    feat(home): add loop of posts

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+if(have_posts()): 
+    while (have_posts()): the_post(); ?>
+        <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+<?php the_content() ?>
+
+<?php endwhile;
+
+else:
+
+    echo '<p>There are no posts!</p>';
+
+endif;
+
+?>
